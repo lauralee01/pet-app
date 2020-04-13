5d064eba17a5fe520da92c144e44c8068b1d07ad
lauralee01 <lauralee285@yahoo.com>
2020-04-13 17:20:07 +0100

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Dogs from '../views/Dogs.vue'
+import Cats from '../views/Cats.vue'
+import Pet from '../views/Pet.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/dogs',
+    name: 'dogs',
+    component: Dogs
+  },
+  {
+    path: '/cats',
+    name: 'cats',
+    component: Cats
+  },
+  {
+    path: '/pets/:species/:id',
+    name: 'pet',
+    component: Pet
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   component: () => import('../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
